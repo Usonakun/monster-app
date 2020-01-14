25301ef7a937e1e090ae9b7b764f432b5fb246e5
uson <uson@usons-MacBook-Pro.local>
2020-01-13 22:38:08 -0500

    Updates

---
+++ precache-manifest.2fd8e4914adf1363758bfd2bf15d17cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9795675130be7b00e62c26a88a928c84",
+    "url": "/Usonakun/monster-project/index.html"
+  },
+  {
+    "revision": "e09585e7d9550d7208f5",
+    "url": "/Usonakun/monster-project/static/css/main.82d4dc59.chunk.css"
+  },
+  {
+    "revision": "efd8eb63cc7299eb4527",
+    "url": "/Usonakun/monster-project/static/js/2.7a0b0f87.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Usonakun/monster-project/static/js/2.7a0b0f87.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e09585e7d9550d7208f5",
+    "url": "/Usonakun/monster-project/static/js/main.ea3c096d.chunk.js"
+  },
+  {
+    "revision": "bcb56579db8b2ba7a7e7",
+    "url": "/Usonakun/monster-project/static/js/runtime-main.96c8209b.js"
+  }
+]);
